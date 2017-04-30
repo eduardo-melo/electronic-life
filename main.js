a960ce1dbcdaa271361b3a63adb1e1ebc1cbf84a
Eduardo <eduardomelo29@gmail.com>
2017-04-30 11:48:34 -0300

    Adds View constructor and prototype functions

--- main.js
+++ main.js
@@ -25,6 +25,17 @@ Grid.prototype.set = function(vector, value){
   this.space[vector.x + this.width * vector.y] = value;
 }
 
+Grid.prototype.forEach = function(f, context){
+  for(var y = 0; y < this.height; y++){
+    for(var x = 0; x < this.width; x++){
+      var value = this.space[x + y * width];
+      if (value != null){
+        f.call(context, value, new Vector(x, y));
+      }
+    }
+  }
+}
+
 var directions = {
   "n" : new Vector(0, -1),
   "ne": new Vector(1, -1),
@@ -99,7 +110,68 @@ World.prototype.toString = function(){
   return output;
 }
 
+World.prototype.turn = function(){
+  var acted = [];
+  this.grid.forEach(function(critter, vector){
+    if (critter.act && acted.indexOf(critter) == -1){
+      acted.push(critter);
+      this.letAct(critter, vector);
+    }
+  }, this);
+}
+
+World.prototype.letAct = function(critter, vector){
+  var action = critter.act(new View(this, vector));
+  if (action && action.type == "move"){
+    var dest = this.checkDestination(action, vector);
+    if(dest && this.grid.get(dest) == null){
+      this.grid.set(vector, null);
+      this.grid.set(dest, critter);
+    }
+  }
+}
+
+World.prototype.checkDestination = function(action, vector){
+  if(directions.hasOwnProperty(action.direction)){
+    var dest = vector.plus(direction[action.direction]);
+
+    if(this.grid.isInside(dest)){
+      return dest;
+    }
+  }
+}
+
 function Wall(){};
+
+function View(world, vector){
+  this.world = world;
+  this.vector = vector;
+}
+
+View.prototype.look = function(dir){
+  var target = this.vector.plus(directions[dir]);
+  if(this.world.grid.isInside(target)){
+    return charFromElement(this.world.grid.get(target));
+  } else {
+    return "#";
+  }
+};
+
+View.prototype.findAll = function(ch){
+  var found = [];
+  for(var dir in directions){
+    if(this.look(dir) == ch){
+      found.push(dir);
+    }
+  }
+  return found;
+};
+
+View.prototype.find = function(ch){
+  var found = this.findAll(ch);
+  if(found.length == 0) return null;
+  return randomElement(found);
+}
 
 var plan = [
   "#####################",
